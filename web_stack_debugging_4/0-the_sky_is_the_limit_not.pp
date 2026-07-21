132bed673c815654aee7cff33d0dcfbc9c193212
Akaliza-ux <m.akaliza@alustudent.com>
2026-07-21 21:50:54 +0000

    Fix Nginx concurrency limit

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,12 @@
-# Raises Nginx connection and file-descriptor limits for heavy traffic.
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/worker_connections 768;/worker_connections 4096;/" /etc/nginx/nginx.conf && sed -i "/worker_processes/a worker_rlimit_nofile 4096;" /etc/nginx/nginx.conf && service nginx restart',
-  unless  => 'grep -q "worker_connections 4096;" /etc/nginx/nginx.conf && grep -q "worker_rlimit_nofile 4096;" /etc/nginx/nginx.conf',
+# Raises the Nginx connection limit to handle concurrent requests.
+exec { 'increase-nginx-connections':
+  command => 'sed -ri "s/worker_connections[[:space:]]+[0-9]+;/worker_connections 4096;/" /etc/nginx/nginx.conf',
   path    => ['/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+  notify  => Exec['restart-nginx'],
+}
+
+exec { 'restart-nginx':
+  command     => 'service nginx restart',
+  path        => ['/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+  refreshonly => true,
 }
