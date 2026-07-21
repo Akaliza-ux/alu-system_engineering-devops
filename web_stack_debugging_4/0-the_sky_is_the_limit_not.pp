2415f6ef52a984d316b89546c5ffadd228bcc5bb
Akaliza-ux <m.akaliza@alustudent.com>
2026-07-21 22:15:47 +0000

    Improve web stack concurrency

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,25 +1,28 @@
-# Raises Nginx and PHP-FPM capacity for concurrent web requests.
-exec { 'increase-nginx-worker-capacity':
-  command => 'sed -ri "s/worker_connections[[:space:]]+[0-9]+;/worker_connections 4096;/" /etc/nginx/nginx.conf',
-  path    => ['/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+# Raise Nginx and PHP-FPM capacity for concurrent requests.
+
+exec { 'increase-worker-connections':
+  command => 'sed -ri "s/worker_connections\\s+[0-9]+;/worker_connections 4096;/" /etc/nginx/nginx.conf',
+  unless  => 'grep -q "worker_connections 4096;" /etc/nginx/nginx.conf',
+  path    => ['/bin','/usr/bin','/usr/sbin'],
   notify  => Exec['restart-nginx'],
 }
 
-exec { 'increase-php-fpm-capacity':
-  command => 'sed -ri "s/^pm.max_children = [0-9]+/pm.max_children = 100/" /etc/php5/fpm/pool.d/www.conf',
+exec { 'increase-pm-max-children':
+  command => 'sed -ri "s/^pm.max_children\\s*=.*/pm.max_children = 100/" /etc/php5/fpm/pool.d/www.conf',
   onlyif  => 'test -f /etc/php5/fpm/pool.d/www.conf',
-  path    => ['/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+  unless  => 'grep -q "^pm.max_children = 100$" /etc/php5/fpm/pool.d/www.conf',
+  path    => ['/bin','/usr/bin','/usr/sbin'],
   notify  => Exec['restart-php-fpm'],
 }
 
 exec { 'restart-nginx':
   command     => 'service nginx restart',
-  path        => ['/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
   refreshonly => true,
+  path        => ['/bin','/usr/bin','/usr/sbin'],
 }
 
 exec { 'restart-php-fpm':
   command     => 'service php5-fpm restart',
-  path        => ['/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
   refreshonly => true,
+  path        => ['/bin','/usr/bin','/usr/sbin'],
 }
