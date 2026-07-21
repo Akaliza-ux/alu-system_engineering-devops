fb1893607b31d31a821b159eb5948536ee37161f
Akaliza-ux <m.akaliza@alustudent.com>
2026-07-21 22:00:58 +0000

    Increase web stack concurrency capacity

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,18 +1,25 @@
-# Raises Nginx capacity and removes restrictive request limits.
+# Raises Nginx and PHP-FPM capacity for concurrent web requests.
 exec { 'increase-nginx-worker-capacity':
   command => 'sed -ri "s/worker_connections[[:space:]]+[0-9]+;/worker_connections 4096;/" /etc/nginx/nginx.conf',
   path    => ['/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
   notify  => Exec['restart-nginx'],
 }
 
-exec { 'remove-nginx-request-limits':
-  command => 'sed -i "/^[[:space:]]*limit_req[[:space:]]/d; /^[[:space:]]*limit_conn[[:space:]]/d" /etc/nginx/sites-enabled/default',
+exec { 'increase-php-fpm-capacity':
+  command => 'sed -ri "s/^pm.max_children = [0-9]+/pm.max_children = 100/" /etc/php5/fpm/pool.d/www.conf',
+  onlyif  => 'test -f /etc/php5/fpm/pool.d/www.conf',
   path    => ['/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
-  notify  => Exec['restart-nginx'],
+  notify  => Exec['restart-php-fpm'],
 }
 
 exec { 'restart-nginx':
   command     => 'service nginx restart',
+  path        => ['/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+  refreshonly => true,
+}
+
+exec { 'restart-php-fpm':
+  command     => 'service php5-fpm restart',
   path        => ['/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
   refreshonly => true,
 }
