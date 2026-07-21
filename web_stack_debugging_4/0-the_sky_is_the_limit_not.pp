9beb000a66d74dd7e59f9466c9dd4b3d5874fdfe
Akaliza-ux <m.akaliza@alustudent.com>
2026-07-21 21:57:33 +0000

    Remove Nginx request limits

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,12 @@
-# Raises the Nginx connection limit to handle concurrent requests.
-exec { 'increase-nginx-connections':
+# Raises Nginx capacity and removes restrictive request limits.
+exec { 'increase-nginx-worker-capacity':
   command => 'sed -ri "s/worker_connections[[:space:]]+[0-9]+;/worker_connections 4096;/" /etc/nginx/nginx.conf',
+  path    => ['/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+  notify  => Exec['restart-nginx'],
+}
+
+exec { 'remove-nginx-request-limits':
+  command => 'sed -i "/^[[:space:]]*limit_req[[:space:]]/d; /^[[:space:]]*limit_conn[[:space:]]/d" /etc/nginx/sites-enabled/default',
   path    => ['/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
   notify  => Exec['restart-nginx'],
 }
