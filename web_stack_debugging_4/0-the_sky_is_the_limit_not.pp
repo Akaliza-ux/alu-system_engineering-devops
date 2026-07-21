ee20d9d42030629a7bdd81d2a3948f60d16225d7
Akaliza-ux <m.akaliza@alustudent.com>
2026-07-21 21:45:51 +0000

    Raise Nginx connection and file limits

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
-# Increases Nginx worker connections to handle concurrent requests.
+# Raises Nginx connection and file-descriptor limits for heavy traffic.
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/worker_connections 768/worker_connections 4096/g" /etc/nginx/nginx.conf && service nginx restart',
+  command => 'sed -i "s/worker_connections 768;/worker_connections 4096;/" /etc/nginx/nginx.conf && sed -i "/worker_processes/a worker_rlimit_nofile 4096;" /etc/nginx/nginx.conf && service nginx restart',
+  unless  => 'grep -q "worker_connections 4096;" /etc/nginx/nginx.conf && grep -q "worker_rlimit_nofile 4096;" /etc/nginx/nginx.conf',
   path    => ['/usr/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'],
 }
